21c55572faa9c433c85488177b2a22764dfd741f
StivenCS <42822488+StivenCS@users.noreply.github.com>
2018-11-17 20:21:21 -0500

    Create,Update Usuario

---
+++ BibliotecaMunicipal/Controladores/UsuarioControlador.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaMunicipal.Controladores
+{
+    public class UsuariosControlador
+    {
+        Modelos.Conexion Con = new Modelos.Conexion();
+        public void InsertarUsuario(Modelos.Usuario us)
+        {
+            try
+            {
+                Con.Conectar();
+                string sql = "INSERT INTO Usuarios VALUES(@doc,@nom,@dir,@tel,@mail,@est) ";
+                SqlCommand sc = new SqlCommand(sql, Con.Conex());
+                sc.Parameters.AddWithValue("@doc", us.UsuDocumento);
+                sc.Parameters.AddWithValue("@nom", us.UsuNombre);
+                sc.Parameters.AddWithValue("@dir", us.UsuDireccion);
+                sc.Parameters.AddWithValue("@tel", us.UsuTelefono);
+                sc.Parameters.AddWithValue("@mail", us.UsuCorreo);
+                sc.Parameters.AddWithValue("@est", us.UsuEstado);
+                int result = sc.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    Console.WriteLine("Usuario agregado con exito!");
+                }
+                else
+                {
+                    Console.WriteLine("El Usuario no pudo ser ingresado!");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Con.Desconectar();
+            }
+        }
+        public void ActualizarUsuario(Modelos.Usuario us)
+        {
+            try
+            {
+                Con.Conectar();
+                string sql = "UPDATE Usuarios Set UsuNombre =  @nom, UsuDireccion = @dir, UsuTelefono = @tel, UsuCorreo = @mail, UsuEstado = @est WHERE UsuDocumento = @doc";
+                SqlCommand sc = new SqlCommand(sql, Con.Conex());
+                sc.Parameters.AddWithValue("@doc", us.UsuDocumento);
+                sc.Parameters.AddWithValue("@nom", us.UsuNombre);
+                sc.Parameters.AddWithValue("@dir", us.UsuDireccion);
+                sc.Parameters.AddWithValue("@tel", us.UsuTelefono);
+                sc.Parameters.AddWithValue("@mail", us.UsuCorreo);
+                sc.Parameters.AddWithValue("@est", us.UsuEstado);
+                sc.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Con.Desconectar();
+            }
+        }
+
+    }
+}
---
+++ BibliotecaMunicipal/Modelos/Usuario.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaMunicipal.Modelos
+{
+    public class Usuario
+    {
+        private string usuDocumento;
+        private string usuNombre;
+        private string usuDireccion;
+        private string usuTelefono;
+        private string usuCorreo;
+        private string usuEstado;
+
+        public string UsuDocumento
+        {
+            get
+            {
+                return usuDocumento;
+            }
+            set
+            {
+                usuDocumento = value;
+            }
+        }
+        public string UsuNombre
+        {
+            get
+            {
+                return usuNombre;
+            }
+            set
+            {
+                usuNombre = value;
+            }
+        }
+        public string UsuDireccion
+        {
+            get
+            {
+                return usuDireccion;
+            }
+            set
+            {
+                usuDireccion = value;
+            }
+        }
+        public string UsuTelefono
+        {
+            get
+            {
+                return usuTelefono;
+            }
+
+            set
+            {
+                usuTelefono = value;
+            }
+        }
+        public string UsuCorreo
+        {
+            get
+            {
+                return usuCorreo;
+            }
+            set
+            {
+                usuCorreo = value;
+            }
+        }
+        public string UsuEstado
+        {
+            get
+            {
+                return usuEstado;
+            }
+            set
+            {
+                usuEstado = value;
+            }
+        }
+    }
+}
