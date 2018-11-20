96a1a463f8607e307399282c15740a4425f8170a
Unknown <casegica@gmail.com>
2018-11-20 16:29:19 -0500

    Model books

---
+++ BibliotecaMunicipal/Modelos/Libro.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaMunicipal.Modelos
+{
+    public class Libro
+    {
+        private string codigo;
+        private string nombre;
+        private string numPag;
+        private string autor;
+        private string editorial;
+        private string area;
+
+        public string Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+
+            set
+            {
+                codigo = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string NumPag
+        {
+            get
+            {
+                return numPag;
+            }
+
+            set
+            {
+                numPag = value;
+            }
+        }
+
+        public string Autor
+        {
+            get
+            {
+                return autor;
+            }
+
+            set
+            {
+                autor = value;
+            }
+        }
+
+        public string Editorial
+        {
+            get
+            {
+                return editorial;
+            }
+
+            set
+            {
+                editorial = value;
+            }
+        }
+
+        public string Area
+        {
+            get
+            {
+                return area;
+            }
+
+            set
+            {
+                area = value;
+            }
+        }
+    }
+}
