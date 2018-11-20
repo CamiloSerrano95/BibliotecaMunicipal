05bf0aa6145b1f1b91a79fd02c6b21d29c7576d1
Unknown <casegica@gmail.com>
2018-11-20 17:40:08 -0500

    Model DetailsPres

---
+++ BibliotecaMunicipal/Modelos/DetallePrestamos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaMunicipal.Modelos
+{
+    public class DetallePrestamos
+    {
+        private string idPrestamo;
+        private string idLibro;
+        private string cantLibro;
+        private string fechaFin;
+        private string fechaVen;
+
+        public string IdPrestamo
+        {
+            get
+            {
+                return idPrestamo;
+            }
+
+            set
+            {
+                idPrestamo = value;
+            }
+        }
+
+        public string IdLibro
+        {
+            get
+            {
+                return idLibro;
+            }
+
+            set
+            {
+                idLibro = value;
+            }
+        }
+
+        public string CantLibro
+        {
+            get
+            {
+                return cantLibro;
+            }
+
+            set
+            {
+                cantLibro = value;
+            }
+        }
+
+        public string FechaFin
+        {
+            get
+            {
+                return fechaFin;
+            }
+
+            set
+            {
+                fechaFin = value;
+            }
+        }
+
+        public string FechaVen
+        {
+            get
+            {
+                return fechaVen;
+            }
+
+            set
+            {
+                fechaVen = value;
+            }
+        }
+    }
+}
