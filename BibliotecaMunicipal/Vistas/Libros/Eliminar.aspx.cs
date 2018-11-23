5689aef38f69feb4a3a8a65c244ca6208a028ee9
Camilo Serrano <casegica@gmail.com>
2018-11-23 12:34:27 -0500

    Delete book

---
+++ BibliotecaMunicipal/Vistas/Libros/Eliminar.aspx.cs
@@ -0,0 +1,38 @@
+﻿using BibliotecaMunicipal.Controladores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BibliotecaMunicipal.Vistas.Libros
+{
+    public partial class Eliminar : System.Web.UI.Page
+    {
+        LibroControlador LC = new LibroControlador();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LlenarLibros();
+            }
+        }
+
+        protected void EliminarLibro_Click(object sender, EventArgs e)
+        {
+            string Codigo = LibrosSelect.SelectedValue;
+
+            LC.EliminarLibro(Codigo);
+        }
+
+        protected void LlenarLibros()
+        {
+            LibrosSelect.DataSource = LC.MostrarLibros();
+            LibrosSelect.DataTextField = "libNombre";
+            LibrosSelect.DataValueField = "libCodigo";
+            LibrosSelect.DataBind();
+            LibrosSelect.Items.Insert(0, new ListItem("Seleccione", "0"));
+        }
+    }
+}
