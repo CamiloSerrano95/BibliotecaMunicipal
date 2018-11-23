21b90b72608eb9e65b276f8fcde86ae427cdee12
Camilo Serrano <casegica@gmail.com>
2018-11-23 12:47:25 -0500

    Consult book

---
+++ BibliotecaMunicipal/Vistas/Libros/Consultar.aspx.cs
@@ -0,0 +1,39 @@
+﻿using BibliotecaMunicipal.Controladores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BibliotecaMunicipal.Vistas.Libros
+{
+    public partial class Consultar : System.Web.UI.Page
+    {
+        LibroControlador LC = new LibroControlador();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LlenarLibros();
+            }
+        }
+
+        protected void ConsultarLibro_Click(object sender, EventArgs e)
+        {
+            string codigo = LibrosSelect.SelectedItem.Value.ToString();
+
+            TablaConsultaLibro.DataSource = LC.ConsultarLibro(codigo);
+            TablaConsultaLibro.DataBind();
+        }
+
+        protected void LlenarLibros()
+        {
+            LibrosSelect.DataSource = LC.MostrarLibros();
+            LibrosSelect.DataTextField = "libNombre";
+            LibrosSelect.DataValueField = "libCodigo";
+            LibrosSelect.DataBind();
+            LibrosSelect.Items.Insert(0, new ListItem("Seleccione", "0"));
+        }
+    }
+}
