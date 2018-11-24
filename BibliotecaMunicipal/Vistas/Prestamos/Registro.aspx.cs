6ed344ccf6c2f37dcbdba06ba0d5c950b583ef32
Camilo Serrano <casegica@gmail.com>
2018-11-24 00:01:56 -0500

    Method for loans

--- BibliotecaMunicipal/Vistas/Prestamos/Registro.aspx.cs
+++ BibliotecaMunicipal/Vistas/Prestamos/Registro.aspx.cs
@@ -9,13 +9,18 @@ namespace BibliotecaMunicipal.Vistas.Prestamos
 {
     public partial class Registro : System.Web.UI.Page
     {
+        Controladores.DetallePrestamosControlador DPC = new Controladores.DetallePrestamosControlador();
         Controladores.UsuarioControlador Cu = new Controladores.UsuarioControlador();
+        Controladores.LibroControlador LC = new Controladores.LibroControlador();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 Listar();
+                ListarLibros();
             }
+
+            MostrarDetalles();
         }
 
         public void Listar()
@@ -32,6 +37,36 @@ namespace BibliotecaMunicipal.Vistas.Prestamos
             Mp.PreFecha = Fecha.Text;
             Mp.PreUsuario = ListaUsuarios.Text;
             Cp.InsrtarPrestamo(Mp);
+
+            IdPrestamo.Text = DPC.CodigoPrestamo();
+
+        }
+
+        protected void ListarLibros()
+        {
+            ListaLibros.DataTextField = "libNombre";
+            ListaLibros.DataValueField = "libCodigo";
+            ListaLibros.DataSource = LC.MostrarLibros("");
+            ListaLibros.DataBind();
+        }
+
+        protected void MostrarDetalles()
+        {
+            TablaDetallesPrestamo.DataSource = DPC.MostrarPrestamos();
+            TablaDetallesPrestamo.DataBind();
+        }
+
+        protected void GuardarDetalles_Click(object sender, EventArgs e)
+        {
+            Modelos.DetallePrestamos DP = new Modelos.DetallePrestamos();
+
+            DP.IdPrestamo = IdPrestamo.Text;
+            DP.IdLibro = ListaLibros.SelectedValue.ToString();
+            DP.CantLibro = Cantidad.Text;
+            DP.FechaFin = FechaEntrega.Text;
+            DP.FechaVen = FechaEntrega.Text;
+
+            MostrarDetalles();
         }
     }
 }
